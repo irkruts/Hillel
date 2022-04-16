317c8d5d25be3436dade2f0c9ef962136c13c756
Iryna Kruts <i_kruts@ukr.net>
2022-04-17 00:33:07 +0300

    change all

--- lesson_3_1.py
+++ lesson_3_1.py
@@ -1,13 +1,7 @@
 def my_range(start, stop, step):
-    if start is not None and stop is not None:
-        new_generator = (start + step for start in range(start, stop))
+    while start <= stop:
+        yield start
         start += step
-        if start < stop:
-            for i in new_generator:
-                yield i
-        yield 'Stop generator. Start point is bigger than stop point'
-    else:
-        yield 'Start point or stop point is None'
 
 
 new_range = my_range(1, 5, 1)
